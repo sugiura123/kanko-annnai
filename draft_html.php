b67dd4a6afe9c2725bc3fc4466f2a5adbb52be53
sugiura123 <sugiura@kumoyanet.com>
2016-10-01 17:40:32 +0900

    Add files via upload

---
+++ draft_html.php
@@ -0,0 +1,21 @@
+<div class="media-left">
+        <img src="place_2.jpg" class="media-object img-thumbnail" align="left" ><br>
+      </div>
+      <div class="media-body">
+        <h4 class="media-heading">日本の城</h4>
+                   日本の城はとても良い所です。日本の城はとても良い所です。
+                   日本の城はとても良い所です。日本の城はとても良い所です。
+                   日本の城はとても良い所です。日本の城はとても良い所です。
+                   日本の城はとても良い所です。日本の城はとても良い所です。
+      </div>
+      <br>
+      <div class="media-left">
+        <img src="place_3.jpg" class="media-object img-thumbnail" align="left">
+      </div>
+      <div class="media-body">
+        <h4 class="media-heading">緑の階段</h4>
+                   旅館の部屋はとても良い所です。旅館の部屋はとても良い所です。
+                   旅館の部屋はとても良い所です。旅館の部屋はとても良い所です。
+                   旅館の部屋はとても良い所です。旅館の部屋はとても良い所です。
+                   旅館の部屋はとても良い所です。旅館の部屋はとても良い所です。
+
